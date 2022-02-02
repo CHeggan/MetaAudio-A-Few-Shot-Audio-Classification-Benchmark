5bb3cdec0f8646bd80b5169114a7f2ccccc0833a
CHeggan <38021095+CHeggan@users.noreply.github.com>
2022-02-02 18:18:31 +0000

    Add files via upload

---
+++ Dataset Processing/Specific Scripts/BC_prune.py
@@ -0,0 +1,176 @@
+"""
+Expeirments with BirdClef require significantly more computational hardware than
+    the other datasets considered. This is primarily due to the incredibly long 
+    samples contained within some of the classes.
+
+In order to make future exprimentation more accessible, we propose pruning the
+    dataset with respect to length of samples. 
+
+To make applying this code more simple, we have it work over an already existing 
+    version of the BirdClef dataset, applying the prune as it iterates through.
+    For added speedup, we allow an option for a csv save of files to be removed etc
+"""
+###############################################################################
+# IMPORTS
+###############################################################################
+import os
+import sys
+import librosa
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+###############################################################################
+# CLASS TIDY UP
+###############################################################################
+# def class_tidy(main_dir, class_thresh):
+#     contained_classes = os.listdir(main_dir)
+#     for seen_class in contained_classes:
+#         path = os.path.join(main_dir, seen_class)
+#         num_samples = len(os.listdir(path))
+#         if num_samples < class_thresh:
+#             for path in 
+#             os.remove()
+#     pass
+
+###############################################################################
+# SAMPLE LENGTH CALC
+###############################################################################
+def load_and_length(data_path, sr):
+    data = np.load(data_path)
+    length = float(data.shape[0]/sr)
+    return length 
+
+###############################################################################
+# MAIN
+###############################################################################
+def main(main_dir, time_thresh_s, class_thresh,  SR, remove, csv_path=None):
+
+    try:
+        bad_files = pd.read_csv(csv_path)
+        already_parsed = True
+
+    except:
+        bad_files = pd.DataFrame(columns=['class', 'file_name'])
+        already_parsed = False
+
+    if already_parsed:
+        for idx in range(bad_files.shape[0]):
+            class_name = bad_files['class'].iloc[idx]
+            file = bad_files['file_name'].iloc[idx]
+            file_path = os.path.join(main_dir, class_name, file)
+
+            if remove:
+                try:
+                    os.remove(file_path)
+                except:
+                    pass
+            
+    else:
+        # Gets the names of the included classes
+        contained_classes = os.listdir(main_dir)
+        # Iterates over the full folder structure and counts how many wav files 
+        total_files = 0
+        for root, _, files in os.walk(main_dir):
+            for f in files:
+                if f.endswith('.npy'):
+                    total_files += 1
+        
+        # A little visual to see how the processing is coming along
+        progress_bar = tqdm(total=total_files)
+
+        num_removed = 0
+        for seen_class in contained_classes:
+                working_dir = os.path.join(main_dir, seen_class)
+
+                for fname in os.listdir(working_dir):
+                    if fname.endswith('.npy'):
+                        file_path = os.path.join(working_dir, fname)
+                        sample_length = load_and_length(file_path, SR)
+
+                        # If sample length is too long, we dont want the sample
+                        if sample_length > time_thresh_s:
+                            new_point = {'class':seen_class, 'file_name':fname}
+                            bad_files = bad_files.append(new_point, ignore_index=True)
+
+                            if remove:
+                                os.remove(file_path)
+                            num_removed += 1
+
+                        # Update the progress bar
+                        progress_bar.update(1)
+
+        # Can only do folder cleaning when we have already removed samples too long
+        if remove:
+            # We perform a class sweep after all of this, where we want a min num samples
+            num_valid = 0
+            for seen_class in contained_classes:
+                class_path = os.path.join(main_dir, seen_class)
+                samples = os.listdir(class_path)
+
+                if len(samples) < class_thresh:
+                    # need to remove all the files individually first before del folder
+                    for file in samples:
+
+                        file_path = os.path.join(main_dir, seen_class, file)
+
+                        new_point = {'class':seen_class, 'file_name':file}
+                        bad_files = bad_files.append(new_point, ignore_index=True)
+
+                        num_removed += 1
+                        os.remove(file_path)
+
+                    # del folder
+                    os.rmdir(os.path.join(main_dir, seen_class))
+                        
+                else:
+                    num_valid += 1
+
+            print('Number of classes Remaining:', num_valid)
+
+        # print('Num Samples Removed:', num_removed)
+        print(bad_files)
+        bad_files.to_csv('remove_files.csv', index=False)
+
+
+main_path = 'D:/Dataset Backups/Datasets/BirdSong/BirdClef2020/Sorted_npy_copy'
+csv_path = 'remove_files.csv'
+
+main(main_dir=main_path,
+    time_thresh_s=180,
+    class_thresh=50,
+    SR=16000,
+    remove=True)
+
+    # #sets working dir as array parent folder
+    # os.chdir(main_dir)
+
+    # # Gets the names of the included classes
+    # contained_classes = os.listdir(main_dir)
+
+    # # Iterates over the full folder structure and counts how many wav files 
+    # total_files = 0
+    # for root, _, files in os.walk(old_dir):
+    #     for f in files:
+    #         if f.endswith('.npy'):
+    #             total_files += 1
+    
+    # # A little visual to see how the processing is coming along
+    # progress_bar = tqdm(total=total_files)
+
+    # for seen_class in contained_classes:
+    #         working_dir = os.path.join(old_dir, seen_class)
+    #         new_working_dir = os.path.join(new_dir, seen_class)
+
+    #         try:
+    #             os.mkdir(new_working_dir)
+    #         except Exception:
+    #             print(f'Already Created: {new_working_dir}')
+
+    #         for fname in os.listdir(working_dir):
+    #             if fname.endswith('.npy'):
+    #                 file_path = os.path.join(working_dir, fname)
+
+    #                 to_spectrogram(file_path, new_working_dir, sample_length, **spec_params)
+    #                 # Update the progress bar
+    #                 progress_bar.update(1)
