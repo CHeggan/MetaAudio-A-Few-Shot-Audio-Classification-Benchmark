81cd881a36a1d7c438f471a571c3e45538dd0512
CHeggan <38021095+CHeggan@users.noreply.github.com>
2022-04-05 11:21:56 +0100

    update

--- All Algorithms/SimpleShot/metric.py
+++
@@ -1,6 +0,0 @@
-###############################################################################
-# CATAGORICAL ACCURACY
-###############################################################################
-def catagorical_accuracy(targets, predictions):
-    predictions = predictions.argmax(dim=1).view(targets.shape)
-    return (predictions == targets).sum().float() / targets.size(0)
